7aa183650adf4a9b5f6d314450578a298bacdbbd
Giu-lis <giulia.domingues1a@gmail.com>
2025-03-26 10:42:02 -0300

    Meu primeiro projeto java

---
+++ OlaJava.java
@@ -0,0 +1,12 @@
+/******************************************************************************
+
+Meu primeiro programa - Olá Java :)
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Olá Java :)");
+	}
+}
