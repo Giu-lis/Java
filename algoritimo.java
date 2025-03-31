9010ea752ec13e3c2918961c64437b9cc36096c2
Giu-lis <giulia.domingues1a@gmail.com>
2025-03-31 14:18:18 -0300

    eu gosto de estudar algoritimos

---
+++ algoritimo.java
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+14
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class algoritimo {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        int m;
+        
+        System.out.println("Digite o quanto você gosta de estudar algoritimos!!!");
+        m = ler.nextInt();
+        
+        for (int i = 0; i <=m; i++ )
+        System.out.println("Eu gosto de estudar algoritimos!!!");
+
+ 
+    }
+}
