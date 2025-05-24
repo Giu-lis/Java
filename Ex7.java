d99ad15e6ade06564caf1674fc308db5d26df4c6
Giu-lis <giulia.domingues1a@gmail.com>
2025-05-24 17:36:46 -0300

    Ex de repetição 07

--- Ex7.java
+++ Ex7.java
@@ -1,35 +1,29 @@
 /******************************************************************************
-Escreva um programa que calcule e exiba o valor da potência de um número informado pelo usuário elevado a um expoente também
-informado pelo usuário, utilizando laços de repetição. Dica: Inicie a variável resultado com o valor 1.
-Ex:
-Base: 2
-Expoente:7 
-Resultado:128
+Leia a idade de cinco pessoas e exiba a soma e a média das idades. 
+
 *******************************************************************************/
 import java.util.Scanner;
 
 public class Ex7 {
 
     public static void main(String[] args) {
-        int resultado;
-        int base;
-        int expoente;
-        Scanner ler;
+        Scanner scanner = new Scanner(System.in);
+        int somaIdades = 0; 
+        int numeroPessoas = 5; 
 
-        resultado = 1;
+        System.out.println("Digite a idade de cinco pessoas: ");
 
-        ler = new Scanner(System.in);
-
-        System.out.println("Digie uma base: ");
-        base = ler.nextInt();
-
-        System.out.println("Digite um expoente: ");
-        expoente = ler.nextInt();
-
-        for (int i = 1; i <= expoente; i++) { 
-            resultado = resultado * base; 
+        for (int i = 1; i <= numeroPessoas; i++) {
+            System.out.print("Idade da pessoa " + i + ": ");
+            int idade = scanner.nextInt(); 
+            somaIdades += idade;
         }
 
-        System.out.println("O resultado é: " + resultado);
+        double mediaIdades = somaIdades / numeroPessoas;
+        System.out.println("\n   Resultados   ");
+        System.out.println("Soma das idades: " + somaIdades);
+        System.out.println("Média das idades: " + mediaIdades);
+
+        scanner.close();
     }
 }
