233da66820b8147235634dfa249e690f5c831bfb
Giu-lis <giulia.domingues1a@gmail.com>
2025-05-28 11:15:16 -0300

    Ex 5 matriz - 28/05

--- Ex5.java
+++ Ex5.java
@@ -1,23 +1,47 @@
 /******************************************************************************
-Faça um programa que calcule e exiba a soma dos números pares de 1 a 100 utilizando um laço de repetição. Utilize while
+Desenvolva um programa que leia duas matrizes 5x2 e crie uma terceira matriz também 5x2
+com o valor da soma dos elementos de mesmo índice.
 *******************************************************************************/
-public class Ex5 {
-
-    public static void main (String[] args) {
-        int soma;
-        int i;
-
-        soma = 0;
-        i = 1;
-
-        while(i <= 100) {
-            if (i % 2 == 0) {
-                soma+=i;
+import java.util.Scanner;
+public class Ex5
+{
+    public static void main(String[] args) {
+        Scanner ler = new Scanner (System.in);
+        int matrizUm [] [] = new int [5][2];
+        int matrizDois [] [] = new int [5][2];
+        int soma [] [] = new int [5][2];
+        
+        
+        System.out.println("Digite os valores para a primeira matriz: ");
+    
+        for (int i = 0; i < matrizUm.length; i++){
+            for (int j = 0; j < matrizUm[i].length; j++){
+                System.out.print("Elemento ["+i+"] ["+j+"]: ");
+                matrizUm[i][j] = ler.nextInt ();
             }
-            i++;
+        }
+        
+        System.out.println();
+        System.out.println("Digite os valores para a segunda matriz: ");
+        
+        for (int i = 0; i < matrizUm.length; i++){
+            for (int j = 0; j < matrizUm[i].length; j++){
+                System.out.print("Elemento ["+i+"] ["+j+"]: ");
+                matrizDois[i][j] = ler.nextInt ();
+            }
         }
 
-        System.out.println("O valor da soma dos números pares de 1 a 100 é: " + soma);
-
+        for (int i = 0; i < matrizUm.length; i++){
+            for (int j = 0; j < matrizUm[i].length; j++){
+                soma [i][j] = matrizUm [i][j] + matrizDois [i][j];
+            }
+        }
+        System.out.println();
+        
+        for (int i = 0; i < matrizUm.length; i++){
+            for (int j = 0; j < matrizUm[i].length; j++){
+                System.out.println("A soma do matriz é: " + soma[i][j]);
+                }
+        }
     }
 }
