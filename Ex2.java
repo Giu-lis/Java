a4859952c7012a63d4ca068a5acae6d239cb8674
Giu-lis <giulia.domingues1a@gmail.com>
2025-05-28 08:29:30 -0300

    Ex 2 matriz - 28/05

--- Ex2.java
+++ Ex2.java
@@ -1,24 +1,36 @@
 /******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
+Crie um algoritmo que leia uma matriz 3x3 e calcule a soma dos valores das colunas da
+matriz.
 *******************************************************************************/
-public class Ex2 {
+import java.util.Scanner;
+public class Ex2
+{
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		int matriz [] []  = new int [3] [3];
+		int somac1 = 0;
+		int somac2 = 0;
+		int somac3 = 0;
+		
+		System.out.println("Digite os elementos da matriz 3x3: ");
+		
+		for (int i = 0; i < matriz.length; i++) {
+		    for (int j = 0; j < matriz[i].length; j++) {
+		        System.out.print("Elemento [" + i + "] [" + j + "]: ");
+		        matriz[i][j] = ler.nextInt();
+		    }
+		    
+		    somac1 += matriz[i][0];
+		    somac2 += matriz[i][1];
+		    somac3 += matriz[i][2];
+		}
+		
+		
+		System.out.println("A soma da coluna 1 é: " + somac1);
+		System.out.println("A soma da coluna 2 é: " + somac2);
+		System.out.println("A soma da coluna 3 é: " + somac3);
+		
 
-    public static void main (String[] args) {
-        int i;
-
-        for (i = 1; i <= 100; i++) {
-            if (i <= 50 && i % 2 == 0) { 
-                System.out.println("Números pares de 1 a 50: - " + i);
-            } else if (i > 50 && i % 2 != 0) { 
-                System.out.println("Números ímpares de 51 a 100 - " + i);
-            }
-        }
-
-        System.out.println();
-        
-    }
+	}
 }
