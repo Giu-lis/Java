0b1ed4d19c96a643e56486d74ca2db9fe62248cb
Giu-lis <giulia.domingues1a@gmail.com>
2025-05-28 08:07:36 -0300

    Ex 1 matriz - 28/05

--- Ex1.java
+++ Ex1.java
@@ -1,20 +1,31 @@
 /******************************************************************************
-Exercicio 001
-Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
-
+1. Crie um programa que leia uma matriz 5x5. Em seguida, conte quantos números pares
+existem na matriz.
 *******************************************************************************/
 import java.util.Scanner;
-
-public class Ex1 {
-
-    public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
-        int num;
-        
-        System.out.println("Insira um número: ");
-        num = ler.nextInt();
-        
-        System.out.println("O número informado foi: " + num);
-        
-    }
+public class Ex1
+{
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		int matriz [] []  = new int [5] [5];
+		int par = 0;
+		
+		System.out.println("Digite os elementos da matriz 5x5: ");
+		
+		for (int i = 0; i < matriz.length; i++) {
+		    for (int j = 0; j < matriz[i].length; j++) {
+		        System.out.print("Elemento [" + i + "] [" + j + "]: ");
+		        matriz[i][j] = ler.nextInt(); 
+		    }
+		}
+		
+		for (int i = 0; i < matriz.length; i++) {
+		    for (int j = 0; j < matriz[i].length; j++) {
+		        if(matriz [i][j]%2 == 0)
+		        par++;
+		    }
+		}
+		System.out.println("A matriz tem essa quantidade de numeros pares: " + par);
+	}
 }
