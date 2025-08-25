e3d419bce032bd5b915120f6251c0f64ae9aef48
Giu-lis <giulia.domingues1a@gmail.com>
2025-08-25 15:32:12 -0300

    25/08 - Enum

---
+++ EnumMeses/MesesDoAno.java
@@ -0,0 +1,16 @@
+package EnumMeses;
+
+public enum MesesDoAno {
+    Janeiro,
+    Fevereiro,
+    Marco,
+    Abril,
+    Maio,
+    Junho,
+    Julho,
+    Agosto,
+    Setmbro,
+    Outubro,
+    Novembro,
+    Dezembro,
+}
---
+++ EnumMeses/ObjetoMesesDoAno.java
@@ -0,0 +1,9 @@
+package EnumMeses;
+
+public class ObjetoMesesDoAno {
+    public static void main (String[] args) {
+        for (MesesDoAno meses: MesesDoAno.values()){
+            System.out.println(meses);
+        }
+    }
+}
